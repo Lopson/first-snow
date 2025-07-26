0799e1ec937b22559b8e1a65236b54e2caa934f2
Lopson <gmdlourenco@gmail.com>
2025-07-27 00:17:40 +0200

    `animated_screenshot_indicator` converted to Python

---
+++ game/lib/animated_screenshot_indicator_ren.py
@@ -0,0 +1,33 @@
+"""renpy
+init -5 python:
+"""
+
+from typing import TYPE_CHECKING
+from time import time
+from renpy.exports.displayexports import restart_interaction
+from renpy.display import screen
+if TYPE_CHECKING:
+    from renpy import store
+    from renpy import config
+
+store._screenshot_taken = 0
+
+
+def screenshot_callback(_) -> None:
+    store._screenshot_taken = time()
+    restart_interaction()
+
+
+def screenshot_overlay() -> None:
+    if not screen.has_screen('screenshot_indicator'):
+        return
+    if not screen.get_screen('screenshot_indicator') and store._screenshot_taken:
+        screen.show_screen('screenshot_indicator')
+    elif (screen.get_screen('screenshot_indicator') and
+            time() - store._screenshot_taken > 4.0):
+        screen.hide_screen('screenshot_indicator')
+        store._screenshot_taken = 0
+
+
+config.screenshot_callback = screenshot_callback # pyright: ignore[reportAttributeAccessIssue]
+config.overlay_functions.append(screenshot_overlay)
