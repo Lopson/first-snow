b7b44a3810b478c347c20826268691c713ae32c5
Lopson <gmdlourenco@gmail.com>
2025-08-03 19:39:30 +0200

    Extracted cue code to appropriate files; created stub for SFX cue notifications

---
+++ game/definitions/sounds.rpy
@@ -0,0 +1,3 @@
+# TODO Actually write down cues for each file. That way, they
+# can be translated if needed later on.
+
--- game/lib/audio_callback_ren.py
+++ game/lib/audio_callback_ren.py
@@ -5,7 +5,9 @@ init -2 python:
 from typing import TYPE_CHECKING, Callable
 from renpy.audio import music
 if TYPE_CHECKING:
-    from renpy import config
+    from renpy import config, persistent
+    from .context_manager_ren import GameContext
+    from .cue_code_ren import cue
 
 _audio_callbacks: dict[str, list[Callable]] = {}
 _audio_state: dict[str, list[Callable]] = {}
@@ -41,3 +43,52 @@ def run_audio_callbacks() -> None:
 
 
 config.periodic_callbacks.append(run_audio_callbacks)
+
+
+def friendly_name(fn: str) -> str:
+    """
+    As far as I can tell, this is just a backup solution
+    in case a music or SFX file has no name defined for it.
+
+    It becomes especially important for SFXs as originally,
+    nobody seems to have wanted to write descriptions down
+    for them.
+    """
+    return (fn
+            # remove directory
+            .rsplit('/', 1)[-1]
+            # remove extension
+            .rsplit('.', 1)[0]
+            # remove separators
+            .replace('_', ' ')
+            .replace('-', ' ')
+            # remove differentiatiors
+            .rstrip('1234567890')
+            )
+
+
+def on_music(fn: str) -> None:
+    if not persistent.cue_music: # pyright: ignore[reportAttributeAccessIssue]
+        return
+    if fn and GameContext.in_playthrough():
+        name: str
+        if tracks.get(fn):  # pyright: ignore[reportUndefinedVariable]
+            name = tracks.get(fn).title # pyright: ignore[reportUndefinedVariable]
+        else:
+            name = friendly_name(fn)
+        cue('icon', 4.0, which='music', txt=name)
+
+
+def on_sound(fn: str) -> None:
+    # TODO Use to-be-written SFX cue strings instead of friendly name.
+    if not persistent.cue_sfx: # pyright: ignore[reportAttributeAccessIssue]
+        return
+    if fn and GameContext.in_playthrough():
+        name = friendly_name(fn)
+        cue('icon', 4.0, which='sfx', txt=name)
+
+
+add_audio_callback('music',   on_music)
+add_audio_callback('sound',   on_sound)
+add_audio_callback('sound2',  on_sound)
+add_audio_callback('loopsfx', on_sound)
