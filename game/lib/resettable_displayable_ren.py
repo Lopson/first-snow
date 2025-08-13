40ce5dd01f6c1c1591ae62aec99b652409d51afb
Lopson <gmdlourenco@gmail.com>
2025-08-13 21:59:34 +0200

    Type hinted file

--- game/lib/resettable_displayable_ren.py
+++ game/lib/resettable_displayable_ren.py
@@ -3,9 +3,11 @@ init -500 python:
 """
 
 
+from typing import Callable
 from renpy.display.displayable import Displayable
 from renpy.display.image import ImageReference, DynamicImage
 from renpy.easy import displayable
+from renpy.display.render import Render, render # pyright: ignore[reportMissingImports]
 
 class ResettableDisplayable(Displayable):
     """
@@ -13,42 +15,43 @@ class ResettableDisplayable(Displayable):
     """
     # Thanks to Asceai on Freenode.
 
-    def __init__(self, child, **kwargs) -> None:
+    def __init__(self, child: Callable, **kwargs) -> None:
         super(ResettableDisplayable, self).__init__(**kwargs)
         self.base = child
         self.child = displayable(self.base())
-        self.st_reset = 0.0
-        self.at_reset = 0.0
-        self.reset = False
+        self.st_reset: float = 0.0
+        self.at_reset: float = 0.0
+        self.reset: bool = False
 
-    def render(self, width, height, st, at):
+    def render(self, width: int, height: int, st: float, at: float) -> Render:
         if self.reset:
             self.child = displayable(self.base())
             self.st_reset = st
             self.at_reset = at
             self.reset = False
 
-        return renpy.render(
+        return render(
             self.child, width, height, st - self.st_reset, at - self.at_reset)
 
     def Reset(self):
-        return SetField(self, "reset", True)
+        return SetField(self, "reset", True) # pyright: ignore[reportUndefinedVariable]
 
     def visit(self) -> list:
         return [ self.child ]
 
-def ResetDisplayable(disp):
+def ResetDisplayable(disp: str):
     targetref = displayable(disp)
+    
     if isinstance(targetref, ImageReference) or isinstance(targetref, DynamicImage):
         targetref.find_target()
-        return targetref.target.Reset()
+        return targetref.target.Reset() # type: ignore
 
 class CappedDisplayable(Displayable):
-    def __init__(self, child, cap, **kwargs):
+    def __init__(self, child: Displayable | str, cap, **kwargs) -> None:
         super(CappedDisplayable, self).__init__(**kwargs)
         self.child = displayable(child)
         self.cap = cap
 
-    def render(self, width, height, st, at):
-        return renpy.render(
+    def render(self, width, height, st, at) -> Render:
+        return render(
             self.child, width, height, min(self.cap, st), min(self.cap, at))
